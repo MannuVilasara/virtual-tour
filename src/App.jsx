28d664ec5b2f9830da830766942e7e126e899ea0
ikeshav26 <keshavgilhotra4@gmail.com>
2026-03-24 23:08:04 +0530

    Fixes for param query

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import { VirtualTour } from './components/VirtualTour';
 
 export default function App() {
-  return <VirtualTour sceneId="5Pd9XFNOX" className="absolute top-0 left-0 w-full h-screen" />;
+  const params = new URLSearchParams(window.location.search);
+  const sceneId = params.get('sceneId') || '5Pd9XFNOX';
+  
+  return <VirtualTour sceneId={sceneId} className="absolute top-0 left-0 w-full h-screen" />;
 }
